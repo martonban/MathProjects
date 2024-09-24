c9099148322012104dcfdcae6b7869055dea62a3
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-09-24 21:30:09 +0200

    Normalized Vector

--- Project/Assets/Scripts/ScriptVector.cs
+++ Project/Assets/Scripts/ScriptVector.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ScriptVector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private void OnDrawGizmos() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector2 ptr = transform.position;
+        float length = Mathf.Sqrt(Mathf.Pow(ptr.x, 2) + Mathf.Pow(ptr.y, 2));
+
+        Gizmos.DrawLine(Vector2.zero , ptr/length);
     }
 }
