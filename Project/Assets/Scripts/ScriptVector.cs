17288a228b01c7cc8747d39bffd1371c8059f7a5
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-12-12 19:02:44 +0100

    Distance

--- Project/Assets/Scripts/ScriptVector.cs
+++ Project/Assets/Scripts/ScriptVector.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 
 public class ScriptVector : MonoBehaviour
 {
+    public Transform aTf;
+    public Transform bTf;
+
+    public float abDist;
+
     private void OnDrawGizmos() {
 
-        Vector2 ptr = transform.position;
-        float length = Mathf.Sqrt(Mathf.Pow(ptr.x, 2) + Mathf.Pow(ptr.y, 2));
+        Vector2 a = aTf.position;
+        Vector2 b = bTf.position;
 
-        Gizmos.DrawLine(Vector2.zero , ptr/length);
+        Gizmos.DrawLine(a, b);
+
+        abDist = Vector2.Distance(a, b);
+
+        // Math Way to normalize a vector
+        //Vector2 ptr = transform.position;
+        //float length = Mathf.Sqrt(Mathf.Pow(ptr.x, 2) + Mathf.Pow(ptr.y, 2));
+
+        //Gizmos.DrawLine(Vector2.zero , ptr/length);
+
+        // Unity Way 
+        Vector2 ptr = transform.position;
+        Vector2 dirToPoint = ptr / ptr.magnitude;
+        Gizmos.DrawLine (Vector2.zero, dirToPoint);
+
     }
 }
