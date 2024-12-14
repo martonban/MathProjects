dcd7807c5084ed4c2dc817e7a485fa9009cf69ed
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-12-14 14:52:19 +0100

    Vector Part DONE

--- Project/Assets/Scenes/Math 2020/Part 1/1_SimpleVector/ScriptVector.cs
+++ Project/Assets/Scenes/Math 2020/Part 1/1_SimpleVector/ScriptVector.cs
@@ -8,6 +8,9 @@ public class ScriptVector : MonoBehaviour
     public Transform aTf;
     public Transform bTf;
 
+    [Range(0f, 4f)]
+    public float offset = 1f;
+
     public float abDist;
 
     private void OnDrawGizmos() {
@@ -15,13 +18,24 @@ public class ScriptVector : MonoBehaviour
         Vector2 a = aTf.position;
         Vector2 b = bTf.position;
 
-        // Draw the direction a to b (GOOD SOLUTION :D)
+        // Draw the direction a to b
         Vector2 direction = (b - a);
         Vector2 direction_noamlized = direction / direction.magnitude;
+
         Gizmos.DrawLine(a, a + direction_noamlized);
 
-        abDist = Vector2.Distance(a, b);
+        Vector2 offsetVector = direction_noamlized * offset;
+        Gizmos.DrawSphere(offsetVector + a, 0.05f);
 
+
+        abDist = Vector2.Distance(a, b);
+        
+
+        
+        //-------------------------------------------------
+        //             Vector Normalization
+        //-------------------------------------------------
+        
         // Math Way to normalize a vector
         //Vector2 ptr = transform.position;
         //float length = Mathf.Sqrt(Mathf.Pow(ptr.x, 2) + Mathf.Pow(ptr.y, 2));
