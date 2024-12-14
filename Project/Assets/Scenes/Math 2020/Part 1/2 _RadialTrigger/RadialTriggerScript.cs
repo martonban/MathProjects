5ed8b0fc86ece0b61fedca82552610a350368538
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-12-14 15:43:58 +0100

    Radial Trigger - WIP

--- Project/Assets/Scenes/Math 2020/Part 1/2 _RadialTrigger/RadialTriggerScript.cs
+++ Project/Assets/Scenes/Math 2020/Part 1/2 _RadialTrigger/RadialTriggerScript.cs
@@ -1,5 +1,30 @@
 using UnityEngine;
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
 
 public class RadialTriggerScript : MonoBehaviour {
-    
+    public Transform objTest;
+    public bool triggered;
+
+    [Range(0f, 4f)]
+    public float radius = 1f;
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        Vector2 origin = transform.position;
+        Vector2 obj_trans = objTest.transform.position;
+
+        float distance = (obj_trans - origin).magnitude;
+
+        if (radius <= distance) {
+            triggered = false;
+        }else {
+            triggered = true;
+        }
+
+        Handles.DrawWireDisc(origin, new Vector3(0,0,1), radius);
+
+    }
+    #endif
 }
