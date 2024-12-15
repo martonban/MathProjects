7e19c6b16dd5fbfcd2ac4d42c086fc6ddfcc7992
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-12-15 15:12:21 +0100

    Radial Trigger - DONE

--- Project/Assets/Scenes/Math 2020/Part 1/2 _RadialTrigger/RadialTriggerScript.cs
+++ Project/Assets/Scenes/Math 2020/Part 1/2 _RadialTrigger/RadialTriggerScript.cs
@@ -15,16 +15,28 @@ public class RadialTriggerScript : MonoBehaviour {
         Vector2 origin = transform.position;
         Vector2 obj_trans = objTest.transform.position;
 
-        float distance = (obj_trans - origin).magnitude;
+        // This is a pretty expensive way to do it
+        float distance = Vector2.Distance(obj_trans, origin);
 
         if (radius <= distance) {
             triggered = false;
             Handles.color = Color.red;
-        }else {
+        } else {
             triggered = true;
             Handles.color = Color.green;
         }
 
+        // You can do this trigger function cheaper:
+        // Note this is not a good way to calculate distance. USE THIS ONLY WHEN YOU WANT TO CHECK IS THE VECTOR IN A CERTAIN THRESHOLD!!
+        Vector2 displacement = obj_trans - origin;
+        float distance2 = displacement.x * displacement.x + displacement.y * displacement.y;
+
+        bool isInside = distance2 < radius * radius;
+
+        // In Unity we have a build in function for it
+        float distance3 = Vector2.SqrMagnitude(obj_trans - origin);
+
+        // Draw Disk
         Handles.DrawWireDisc(origin, new Vector3(0,0,1), radius);
 
     }
