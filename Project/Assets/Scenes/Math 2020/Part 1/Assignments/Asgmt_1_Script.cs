9d9df31a88f38153ec458cb34fafb8ec6fb45be6
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-12-16 15:40:11 +0100

    Assignment 1 - DONE

---
+++ Project/Assets/Scenes/Math 2020/Part 1/Assignments/Asgmt_1_Script.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+
+/*
+ *-------------------------------------------------------------------------------------
+ *  In this Assignment I have to recreate the radial trigger function from the 
+ *  course on my own. 
+ *-------------------------------------------------------------------------------------
+ */
+
+
+public class Asgmt_1_script : MonoBehaviour
+{
+    // Object I want to check
+    public Transform obj;
+
+    // Radius
+    [Range(0f, 10f)]
+    public float radius = 1f;
+
+    private void OnDrawGizmos() {
+        Vector2 origin = this.transform.position;
+        Vector2 obj_trs = obj.transform.position;
+        bool trigger = false;
+        Handles.color = Color.red;
+
+        // Distance Calculation
+        float distance = Vector2.Distance(origin, obj_trs);
+
+        trigger = distance <= radius;
+
+        if (trigger) {
+            Handles.color = Color.green;
+        }
+
+        Handles.DrawWireDisc(origin, new Vector3(0, 0, 1), radius);
+    }
+}
