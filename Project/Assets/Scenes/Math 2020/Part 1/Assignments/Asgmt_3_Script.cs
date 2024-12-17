2d4242a477b43f60a5d1ecbae128b1b7b416edb9
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-12-17 15:56:04 +0100

    Assignment 3 init

---
+++ Project/Assets/Scenes/Math 2020/Part 1/Assignments/Asgmt_3_Script.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Asgmt_3_Script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
