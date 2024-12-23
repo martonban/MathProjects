9830d57183e9ef776ab9a0516e6121d2260580a8
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-12-23 17:27:22 +0100

    Assignment 2 Solution by the course

--- Project/Assets/Scenes/Math 2020/Part 2/Assignments_Solution_by_Course/Asg2_Script.cs
+++ Project/Assets/Scenes/Math 2020/Part 2/Assignments_Solution_by_Course/Asg2_Script.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 
-public class Asg2_Script : MonoBehaviour
-{
+public class Asg2_Script : MonoBehaviour {
 
+    [Range(0f, 1f)]
+    public float precisness = 05f;
+
+    public Transform objTf;
+
+    private void OnDrawGizmos() {
+        Vector2 center = transform.position;
+        Vector2 playerPos = objTf.position;
+        Vector2 playerLookDir = objTf.right; // x-axis
+
+        Vector2 playerToTriggerDir = (center - playerPos).normalized;
+
+        float lookness = Vector2.Dot(playerLookDir, playerToTriggerDir);
+
+        bool IsLookingTrigger = lookness >= precisness; 
+
+
+        Gizmos.color = IsLookingTrigger ? Color.green : Color.red;
+        Gizmos.DrawLine(playerPos, playerPos + playerToTriggerDir);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(playerPos, playerPos + playerLookDir);
+    }
 }
