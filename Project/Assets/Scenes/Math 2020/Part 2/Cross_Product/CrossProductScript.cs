3b458c48cf0f23cf387554746858ecb819331447
Márton Bán <30813844+martonban@users.noreply.github.com>
2025-01-04 16:53:30 +0100

    video DONE

--- Project/Assets/Scenes/Math 2020/Part 2/Cross_Product/CrossProductScript.cs
+++ Project/Assets/Scenes/Math 2020/Part 2/Cross_Product/CrossProductScript.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEditor;
 
 public class CrossProductScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private void OnDrawGizmos() {
+        Vector3 headPos = transform.position;
+        Vector3 lookDir = transform.forward;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void DrawRay (Vector3 p, Vector3 dir) => Handles.DrawAAPolyLine(p, p + dir);
+
+        if (Physics.Raycast(headPos, transform.forward, out RaycastHit hit)) {
+            Vector3 hitPos = hit.point;
+            Vector3 normal = hit.normal;
+            Vector3 right = Vector3.Cross(lookDir, normal).normalized;
+            Vector3 forward = Vector3.Cross(right, normal);
+
+            // Visualize hit ray
+            Handles.color = Color.white;
+            Handles.DrawAAPolyLine(headPos, hitPos);
+
+            // Visualize surface normal vector
+            Handles.color = Color.green;
+            DrawRay(hitPos, right);
+
+            // Visualize right vector
+            Handles.color = Color.red;
+            DrawRay(hitPos, normal);
+
+            // Visualize forward vector
+            Handles.color = Color.cyan;
+            DrawRay(hitPos, forward);
+        } else {
+            Handles.color = Color.red;
+            DrawRay(headPos, lookDir);
+        }
     }
 }
